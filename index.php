19a325ff227e276ed5d3d94cff73ddf88dc866a5
Hellios3000 <Skeptic.q@gmail.com>
2015-07-15 18:37:14 +0300

    qww

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include ('user.php');
 if($_POST){
 	if(isset($_POST['login'])){						//нажата кнопка с name="login"
 		$user = new User();
-		$user->login($_POST);
+		echo $user->login($_POST);
 	}elseif(isset($_POST['register'])){
 		$user = new User();
 		$user->register($_POST);
@@ -27,7 +27,16 @@ if($_POST){
 <form method="POST">
 	<input name="name"><br>
 	<input name="password" type="password"><br>
-	<input name="login" type="submit" value="Войти"><br>
+	<input type="submit" name="login" value="Войти"><br>
 	<input name="email"><br>
 	<input type="submit" name="register" value="Регистрация"><br>
 </form>
+
+POST:<pre>
+<?php print_r($_POST)?> 
+</pre>
+<br>
+$_SESSION:
+<pre>
+<?php print_r($_SESSION)?> 
+</pre>
--- user.php
+++ user.php
@@ -15,18 +15,17 @@ class user extends Mysqli{
 		parent::__construct($config['server'], $config['user'], $config['password'], $config['database']);
 	}
 	
-	function login($data){
-		$query = $this->query("SELECT * FROM `users` WHERE `login` = '{$data['name']}' LIMIT 0 , 50");
-		
+	function login($data){                                                  //блять, где у тебя есть $name?!?!?!?!
+		$query = $this->query("SELECT * FROM `users` WHERE `login` = '{$data['name']}' LIMIT 1");
+				
 		if($query->num_rows >= 1){
 			$password = md5($data['password']);
 			
-			while($row = $query->fetch_assoc){
+			while($row = $query->fetch_assoc()){
 				if($row['password'] == $password){
-					$_SESSION['userID'] = $row['id'];
-				}
-				
-				return 'Привет, '.$row['login'].'!';
+					$_SESSION['user_id'] = $row['id'];
+					return 'Привет, '.$row['login'].'!';
+				}				
 			}
 		}else{
 			return 'Такого пользователя нет в бд!';
